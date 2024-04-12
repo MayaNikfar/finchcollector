d1977c17f69dd3c850e59ae6f56ab2b3f4b329df
MayaNikfar <nikfarmaya@email.com>
2024-04-12 11:24:48 +1000

    commit Finch collector Lab 4

--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,39 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+) 
 
 class Finch(models.Model):
-  name = models.CharField(max_length=100)
-  breed = models.CharField(max_length=100)
+  name = models.CharField(max_length=256)
+  breed = models.CharField(max_length=256)
   description = models.TextField(max_length=256)
  
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'finch_id': self.id})
+  
   def __str__(self):
     return f'{self.name} ({self.id})'
 
-  def get_absolute_url(self):
-    return reverse('detail', kwargs={'finch_id': self.id})
+ 
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  finch = models.ForeignKey(
+    Finch,
+    on_delete=models.CASCADE
+  )
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
