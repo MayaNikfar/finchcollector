0a311144bee673379fb9afa9b29661fa1e9d0ba6
MayaNikfar <nikfarmaya@email.com>
2024-04-12 13:07:06 +1000

    commit Finch collector Lab 5(Final)

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
   ('B', 'Breakfast'),
@@ -7,17 +8,30 @@ MEALS = (
   ('D', 'Dinner')
 ) 
 
+# Create your models here.
+class Toy(models.Model):
+  name = models.CharField(max_length=256)
+  color = models.CharField(max_length=20)
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+  
+  def __str__(self):
+    return self.name
+  
 class Finch(models.Model):
   name = models.CharField(max_length=256)
   breed = models.CharField(max_length=256)
   description = models.TextField(max_length=256)
- 
+  toys = models.ManyToManyField(Toy)
   def get_absolute_url(self):
     return reverse('detail', kwargs={'finch_id': self.id})
   
   def __str__(self):
     return f'{self.name} ({self.id})'
 
+def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
  
 class Feeding(models.Model):
   date = models.DateField('Feeding Date')
